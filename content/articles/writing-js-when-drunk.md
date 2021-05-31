8a078c2ea4d6436739937e610e1e9bf8c9a8e173
Tobias Berge <t.berge@shopware.com>
2021-05-31 23:47:30 +0200

    2nd article

---
+++ content/articles/writing-js-when-drunk.md
@@ -0,0 +1,15 @@
+---
+title: Writing JavaScript when you're drunk
+description: Lorem ipsum dolor sit amet
+img: first-blog-post.jpg
+alt: my first blog post
+author:
+  name: Tobbe
+  bio: All about Tobbe
+  image: https://images.unsplash.com/photo-1533636721434-0e2d61030955?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80
+tags:
+- JavaScript
+- ketchup
+---
+
+Hoppala
--- pages/talks.vue
+++ pages/talks.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="container">
-    <h1>My talks</h1>
-  </div>
+  <Page title="My talks">
+    <h3>Upcoming talks</h3>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Talk title</th>
+          <th>Conference / Event</th>
+          <th>Date / Slot</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>How to survive conferences as an introvert</td>
+          <td>JSConf</td>
+          <td>14.09.2021</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h3>Talks I held in the past</h3>
+  </Page>
 </template>
 
 <script>
@@ -11,5 +30,20 @@ export default {
 </script>
 
 <style scoped>
+  table {
+    width: 100%;
+    border: 1px solid var(--color-border);
+    margin-bottom: 20px;
+    border-collapse: collapse;
+  }
 
+  table th {
+    text-align: left;
+    background-color: var(--color-background-light);
+  }
+
+  table th,
+  table td {
+    padding: 12px;
+  }
 </style>
