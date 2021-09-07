96aa447ca890b8e1c8dc9fcd707afc5d9c9b657c
Tobias Berge <t.berge@shopware.com>
2021-09-07 21:42:51 +0200

    Use scss for button component

--- components/Button.vue
+++ components/Button.vue
@@ -21,37 +21,32 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .button {
   display: inline-block;
-  color: #fff;
+  color: var(--bg);
   text-decoration: none;
   background-color: var(--color-primary);
   font-family: var(--font-family-default);
   font-size: 16px;
   padding: 12px 30px;
   cursor: pointer;
-
   border: solid 3px var(--color-primary);
   border-top-left-radius: 255px 15px;
   border-top-right-radius: 15px 255px;
   border-bottom-right-radius: 225px 15px;
   border-bottom-left-radius:15px 255px;
 
-}
-
-.button--secondary {
-  background-color: transparent;
-  color: var(--color-primary);
-  padding: 11px 30px;
-
-  bottom: 0;
-  /*margin: 50px auto 0 auto;*/
-  border: solid 3px var(--color-primary);
-
-  border-top-left-radius: 255px 15px;
-  border-top-right-radius: 15px 255px;
-  border-bottom-right-radius: 225px 15px;
-  border-bottom-left-radius:15px 255px;
+  &--secondary {
+     background-color: transparent;
+     color: var(--color-primary);
+     padding: 11px 30px;
+     bottom: 0;
+     border: solid 3px var(--color-primary);
+     border-top-left-radius: 255px 15px;
+     border-top-right-radius: 15px 255px;
+     border-bottom-right-radius: 225px 15px;
+     border-bottom-left-radius:15px 255px;
+   }
 }
 </style>
