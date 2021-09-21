7930a2d5f6d6520ba6d260d27e7d91716fa1402b
leichteckig <29896429+leichteckig@users.noreply.github.com>
2021-09-21 22:06:26 +0200

    Fix redirect issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  generate: {
+    fallback: true
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
