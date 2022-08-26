72c4d3dda0b03d51bd10aca553b099af6ac82efa
leichteckig <29896429+leichteckig@users.noreply.github.com>
2022-08-26 20:13:28 +0200

    Update to Cypress 10

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,26 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   video: false,
-  projectId: 'jr7kjx',
+  projectId: "jr7kjx",
+
   env: {
     percy: false,
   },
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
   },
-})
+
+  component: {
+    devServer: {
+      framework: "nuxt",
+      bundler: "webpack",
+    },
+  },
+});
